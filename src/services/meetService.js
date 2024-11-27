7f8a7e22009064a08d84b745a2f12b79ab67d917
Charu Thomas <charu@getox.com>
2024-11-26 20:13:23 -0500

    mediastream pt2

--- src/services/meetService.js
+++ src/services/meetService.js
@@ -1,4 +1,5 @@
 // src/services/meetService.js
+
 class MeetService {
   constructor() {
     this.state = {
@@ -15,25 +16,114 @@ class MeetService {
     // Create a broadcast channel for cross-interface communication
     this.channel = new BroadcastChannel('yuzu-meet');
     this.channel.onmessage = (event) => {
-      if (event.data.type === 'VIDEO_TOGGLE') {
-        this._handleVideoToggle(event.data.isVideoOff);
+      switch (event.data.type) {
+        case 'VIDEO_TOGGLE':
+          this._handleVideoToggle(event.data.isVideoOff);
+          break;
+        case 'AUDIO_TOGGLE':
+          this._handleAudioToggle(event.data.isMuted);
+          break;
+        case 'END_MEETING':
+          this._handleEndMeeting();
+          break;
       }
     };
+  }
+
+  _handleVideoToggle(isVideoOff) {
+    if (this._mediaStream) {
+      const videoTrack = this._mediaStream.getVideoTracks()[0];
+      if (videoTrack) {
+        videoTrack.enabled = !isVideoOff;
+        this.updateState({ isVideoOff });
+      }
+    }
+  }
+
+  _handleAudioToggle(isMuted) {
+    if (this._mediaStream) {
+      const audioTrack = this._mediaStream.getAudioTracks()[0];
+      if (audioTrack) {
+        audioTrack.enabled = !isMuted;
+        this.updateState({ isMuted });
+      }
+    }
+  }
+
+  _handleEndMeeting() {
+    this.cleanup();
   }
 
   async initializeMediaStream() {
     try {
       if (!this._mediaStream) {
+        console.log('Creating new MediaStream');
         this._mediaStream = await navigator.mediaDevices.getUserMedia({
           audio: true,
           video: true
         });
-        console.log('Created new MediaStream');
+        
+        // Set initial states based on localStorage
+        const videoTrack = this._mediaStream.getVideoTracks()[0];
+        const audioTrack = this._mediaStream.getAudioTracks()[0];
+        
+        if (videoTrack) {
+          videoTrack.enabled = !this.state.isVideoOff;
+        }
+        if (audioTrack) {
+          audioTrack.enabled = !this.state.isMuted;
+        }
       }
       return this._mediaStream;
     } catch (error) {
       console.error('Error initializing media stream:', error);
       throw error;
+    }
+  }
+
+  toggleVideo = async (source) => {
+    try {
+      await this.initializeMediaStream();
+      const newIsVideoOff = !this.state.isVideoOff;
+      
+      this.channel.postMessage({
+        type: 'VIDEO_TOGGLE',
+        isVideoOff: newIsVideoOff
+      });
+      
+      this._handleVideoToggle(newIsVideoOff);
+      console.log(`Video toggled by ${source}, isVideoOff: ${newIsVideoOff}`);
+    } catch (error) {
+      console.error('Error toggling video:', error);
+    }
+  }
+
+  toggleMute = async (source) => {
+    try {
+      await this.initializeMediaStream();
+      const newIsMuted = !this.state.isMuted;
+      
+      this.channel.postMessage({
+        type: 'AUDIO_TOGGLE',
+        isMuted: newIsMuted
+      });
+      
+      this._handleAudioToggle(newIsMuted);
+      console.log(`Audio toggled by ${source}, isMuted: ${newIsMuted}`);
+    } catch (error) {
+      console.error('Error toggling audio:', error);
+    }
+  }
+
+  endMeeting = async () => {
+    try {
+      this.channel.postMessage({
+        type: 'END_MEETING'
+      });
+      
+      this._handleEndMeeting();
+    } catch (error) {
+      console.error('Error ending meeting:', error);
     }
   }
 
@@ -63,52 +153,6 @@ class MeetService {
     }
   }
 
-  _handleVideoToggle(isVideoOff) {
-    if (this._mediaStream) {
-      const videoTrack = this._mediaStream.getVideoTracks()[0];
-      if (videoTrack) {
-        videoTrack.enabled = !isVideoOff;
-        this.updateState({ isVideoOff });
-      }
-    }
-  }
-
-  toggleVideo = async (source) => {
-    try {
-      await this.initializeMediaStream();
-      const newIsVideoOff = !this.state.isVideoOff;
-      
-      // Broadcast the change to all interfaces
-      this.channel.postMessage({
-        type: 'VIDEO_TOGGLE',
-        isVideoOff: newIsVideoOff
-      });
-      
-      // Handle locally
-      this._handleVideoToggle(newIsVideoOff);
-      
-      console.log(`Video toggled by ${source}, isVideoOff: ${newIsVideoOff}`);
-    } catch (error) {
-      console.error('Error toggling video:', error);
-    }
-  }
-
-  toggleMute = async (source) => {
-    try {
-      const stream = await this.initializeMediaStream();
-      const audioTrack = stream.getAudioTracks()[0];
-      
-      if (audioTrack) {
-        const newIsMuted = !this.state.isMuted;
-        audioTrack.enabled = !newIsMuted;
-        this.updateState({ isMuted: newIsMuted });
-        console.log(`Audio toggled by ${source}, isMuted: ${newIsMuted}`);
-      }
-    } catch (error) {
-      console.error('Error toggling audio:', error);
-    }
-  }
-
   connectDevice = (deviceType) => {
     this.state.connectedDevices.add(deviceType);
     this.updateState({ connectedDevices: this.state.connectedDevices });
@@ -121,15 +165,15 @@ class MeetService {
     console.log(`Device disconnected: ${deviceType}`);
   }
 
+  updateState = (newState) => {
+    this.state = { ...this.state, ...newState };
+    this.subscribers.forEach(callback => callback(this.state));
+  }
+
   subscribe = (callback) => {
     this.subscribers.add(callback);
     callback(this.state);
     return () => this.subscribers.delete(callback);
-  }
-
-  updateState = (newState) => {
-    this.state = { ...this.state, ...newState };
-    this.subscribers.forEach(callback => callback(this.state));
   }
 
   cleanup = () => {
